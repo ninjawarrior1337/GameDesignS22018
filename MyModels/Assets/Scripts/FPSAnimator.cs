6af5ac91db55aa2d003cba3c738118270c764e82
unknown <118882@ecrchs.org>
2018-03-19 09:57:04 -0700

    Started to animate fps ctrler

---
+++ MyModels/Assets/Scripts/FPSAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSAnimator : MonoBehaviour {
+
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		animator.SetBool("Walking", Input.GetKey(KeyCode.W));
+		animator.SetBool("Backwards", Input.GetKey(KeyCode.S));
+		animator.SetBool("Running", Input.GetKey(KeyCode.LeftShift));
+	}
+}
