fcae69c26614d6a9e4e6fab6a8eb0d1200c3ce12
unknown <118882@ecrchs.org>
2018-03-20 09:43:15 -0700

    Finished fps animation

--- MyModels/Assets/Scripts/FPSAnimator.cs
+++ MyModels/Assets/Scripts/FPSAnimator.cs
@@ -15,5 +15,22 @@ public class FPSAnimator : MonoBehaviour {
 		animator.SetBool("Walking", Input.GetKey(KeyCode.W));
 		animator.SetBool("Backwards", Input.GetKey(KeyCode.S));
 		animator.SetBool("Running", Input.GetKey(KeyCode.LeftShift));
+
+		if (animator.GetBool("Running") && animator.GetBool("Backwards"))
+		{
+			animator.SetBool("RunningBackwards", true);
+			animator.SetBool("Backwards", false);
+			animator.SetBool("Running", false);
+		}
+		else
+			animator.SetBool("RunningBackwards", false);
+
+		if(animator.GetBool("Running") && !animator.GetBool("Walking"))
+		{
+			animator.SetBool("Running", false);
+		}
+
+		if(Input.GetKey(KeyCode.Space))
+			animator.SetTrigger("Jump");
 	}
 }
